1f6a62bb7de65e60f17ac0e599fabbe8b54f1070
Andy Ye <andyye333@gmail.com>
2016-06-26 15:29:40 -0500

    Started Logic Class

--- src/Logic.java
+++ src/Logic.java
@@ -1,4 +1,14 @@
 
 public class Logic {
-
+	
+	
+	private String name;
+	
+	public Logic(String n) {
+		name = n;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
 }
