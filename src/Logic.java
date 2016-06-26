2b19be0966bf0588cd11448dafff285c55cd34f7
Andy Ye <andyye333@gmail.com>
2016-06-26 15:52:32 -0500

    delete edit from github

--- src/Logic.java
+++ src/Logic.java
@@ -11,7 +11,6 @@ public class Logic {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("hi");
-		System.out.println("edit from github");
 		System.out.println("edit from github 2");
 
 	}
